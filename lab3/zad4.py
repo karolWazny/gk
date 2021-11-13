e5f44079cf80c481515b6e68cba2ce679aa694e2
karolWazny <63009342+karolWazny@users.noreply.github.com>
2021-11-13 19:27:32 +0100

    zadanie 4 lab 3

---
+++ lab3/zad4.py
@@ -0,0 +1,17 @@
+import numpy
+
+from boilerplate import *
+import common
+
+
+class ThirdRenderer(Renderer):
+    def __init__(self):
+        self.egg = common.Egg(samples=15, scaling=8, translation=common.Point(0, -35, 0), mode="lines")
+
+    def render(self, time):
+        common.axes()
+        self.egg.draw()
+        common.spin(numpy.sqrt(time) * 0.05)
+
+if __name__ == '__main__':
+    main(ThirdRenderer())
